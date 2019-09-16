58c41ce2d1197d1a6d96d875b5e57ebf4d0f5b98
rilwan <olawuyirilwan98@gmail.com>
2019-09-16 15:45:35 +0100

    initial commit

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <input type="text" placeholder="email">
+    <input type="password" placeholder="password">
+    <button>Register</button>
+</div>
+</template>
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+export default{
+    data(){
+        return{
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+       async Register(){
+            const response = await AuthenticationService.register({
+            email:this.email,
+            password:thispassword
+        })
+       }
+    }
+}
+</script>
